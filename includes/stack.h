745fb4299a37d5c4148c2afa90740e1bdb5aaf1d
Ricardo Cardante <rcsafe@tutanota.de>
2017-01-30 13:23:05 +0000

    Added generic Stack implementation and correspondent API (similar to Java's API)

--- includes/stack.h
+++ includes/stack.h
@@ -1,4 +1,46 @@
 #ifndef STACK_HEADER
 #define STACK_HEADER
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "bool.h"
+
+#define CAPACITY 100
+#define FULL_STACK -1
+#define EMPTY_STACK -2
+#define NOT_FOUND -3
+#define SUCCESS 0
+
+typedef struct stack *Stack;
+
+/* Function that compares stack elements. Returns -1 if A<B, 0 if A==B or 1 if A>B */
+typedef int (*StackElementComparator)(void*, void*);
+
+/* Creates a new Stack, with capacity for CAPACITY elements. */
+Stack stack_create();
+
+/* Pushes an item onto the top of this stack.
+   Returns SUCCESS if stack not full, FULL_STACK otherwise.
+*/
+int push (Stack, void *);
+
+/* Removes the object at the top of this stack.
+   Returns SUCCESS if stack not empty, EMPTY_STACK otherwise
+*/
+int pop (Stack);
+
+/* Looks at the object at the top of this stack without removing it from the stack. */
+void *peek (Stack);
+
+/* Returns stack current size. */
+int size (Stack);
+
+/* Returns the 1-based position where an item is on this stack.
+   Returns NOT_FOUND if the element is not on this stack.
+*/
+int search (Stack, void *, StackElementComparator);
+
+/* Tests if this stack is empty. */
+bool empty (Stack);
+
 #endif
--- src/stack.c
+++ src/stack.c
@@ -1,1 +1,71 @@
-#include <stack.h>
+/*
+    Generic Stack implementation.
+*/
+#include "stack.h"
+
+struct stack {
+  void *values[CAPACITY];
+  int nr_elems;
+};
+
+/* Creates a new Stack, with capacity for CAPACITY elements. */
+Stack stack_create (void) {
+  Stack s = (Stack) malloc (sizeof (struct stack) );
+  if (s == NULL)
+    return NULL;
+  s -> nr_elems = 0;
+  return s;
+}
+
+/* Pushes an item onto the top of this stack.
+   Returns SUCCESS if stack not full, FULL_STACK otherwise.
+*/
+int push (Stack s, void *value) {
+  if (s -> nr_elems == CAPACITY) {
+      return FULL_STACK;
+  }
+  else {
+    s -> values[s -> nr_elems++] = value;
+    return SUCCESS;
+  }
+}
+
+/* Removes the object at the top of this stack.
+   Returns SUCCESS if stack not empty, EMPTY_STACK otherwise
+*/
+int pop (Stack s) {
+  if (s -> nr_elems == 0) {
+    return EMPTY_STACK;
+  }
+  else {
+    s -> nr_elems--;
+    return SUCCESS;
+  }
+}
+
+/* Looks at the object at the top of this stack without removing it from the stack. */
+void *peek (Stack s) {
+  return s -> values[(s -> nr_elems) - 1];
+}
+
+/* Returns stack current size. */
+int size (Stack s) {
+  return s -> nr_elems;
+}
+
+/* Returns the 1-based position where an item is on this stack.
+   Returns NOT_FOUND if the element is not on this stack.
+*/
+int search (Stack s, void *value, StackElementComparator comparator) {
+  Stack aux = s;
+  for (int i = 0; i < aux -> nr_elems; i++) {
+    if ( comparator ( s -> values[i], value ) == 0 )
+      return i;
+  }
+  return NOT_FOUND;
+}
+
+/* Tests if this stack is empty. */
+bool empty (Stack s) {
+  return ( s -> nr_elems == 0 );
+}
