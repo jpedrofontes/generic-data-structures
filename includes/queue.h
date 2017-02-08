504fa4010f2f03c432617f6fda1639550f4f7f47
João Pedro Fontes <jpedrofontes8@gmail.com>
2017-02-08 11:56:01 +0000

    Added new functions on Queue and adapted some others

--- includes/queue.h
+++ includes/queue.h
@@ -2,25 +2,50 @@
 #define QUEUE_HEADER
 
 #include <stdlib.h>
+#include "bool.h"
 
+#define CAPACITY 100
 #define FULL_QUEUE -1
 #define EMPTY_QUEUE -2
+#define NOT_FOUND -3
+#define SUCCESS 0
 
+typedef void* QueueElement;
 typedef struct queue *Queue;
 
-/* Creates a new queue with the provided size. */
-Queue queue_create(int);
+/* Function that compares queue elements. Returns -1 if A<B, 0 if A==B or 1 if A>B */
+typedef int (*QueueElementComparator)(QueueElement, QueueElement);
 
-/* Destroy the queue. */
-void queue_destroy();
+/* Creates a new Queue, with capacity for CAPACITY elements. */
+Queue queue_create(QueueElementComparator);
 
-/* Put a new value in queue's tail. */
-int enqueue(Queue, void*);
+/* Destroys the queue. */
+void queue_destroy(Queue);
 
-/* Retrieve the value in queue's head. */
-void* dequeue(Queue);
+/* Pushes an item onto the top of this queue.
+   Returns SUCCESS if queue not full, FULL_STACK otherwise.
+*/
+int enqueue(Queue, QueueElement);
 
-/* Returns the number of elements in the queue. */
+/* Retrieves, but does not remove, the head of this queue,
+   or returns null if this queue is empty.
+*/
+QueueElement peek(Queue);
+
+/* Retrieves and removes the head of this queue,
+   or returns null if this queue is empty.
+*/
+QueueElement dequeue(Queue);
+
+/* Returns queue current size. */
 int size(Queue);
+
+/* Returns the 1-based position where an item is on this queue.
+   Returns NOT_FOUND if the element is not on this queue.
+*/
+int search(Queue, QueueElement);
+
+/* Tests if this queue is empty. */
+bool empty(Queue);
 
 #endif
--- src/queue.c
+++ src/queue.c
@@ -1,27 +1,29 @@
 #include "queue.h"
 
 struct queue {
-	void** values;
+	QueueElement* values;
 	int head;
 	int tail;
 	int size;
 	int nr_elems;
+	QueueElementComparator comparator;
 };
 
 /* Creates a new queue with the provided size. */
-Queue queue_create(int size) {
+Queue queue_create(QueueElementComparator c) {
 	Queue q = malloc(sizeof(struct queue));
 	if (q == NULL)
 		return NULL;
-	q->values = malloc(size * sizeof(void*));
+	q->values = malloc(CAPACITY * sizeof(QueueElement));
 	if (q->values == NULL) {
 		free(q);
 		return NULL;
 	}
 	q->head = 0;
 	q->tail = 0;
-	q->size = size;
+	q->size = CAPACITY;
 	q->nr_elems = 0;
+	q->comparator = c;
 	return q;
 }
 
@@ -33,31 +35,62 @@ void queue_destroy(Queue q) {
 	}
 }
 
-/* Put a new value in queue's tail. */
-int enqueue(Queue q, void* value) {
-	if (q->nr_elems == q->size)
+/* Pushes an item onto the top of this queue.
+   Returns SUCCESS if queue not full, FULL_STACK otherwise.
+*/
+int enqueue(Queue q, QueueElement value) {
+	if ( q->nr_elems == q->size )
 		return FULL_QUEUE;
-	q->values[q->tail++] = value;
+	q->values[ q->tail++ ] = value;
 	q->tail %= q->size;
 	q->nr_elems++;
-	return 0;
+	return SUCCESS;
 }
 
-/* Retrieve the value in queue's head. */
-void* dequeue(Queue q) {
+/* Retrieves, but does not remove, the head of this queue,
+   or returns null if this queue is empty.
+*/
+QueueElement peek(Queue q) {
 	if (q->nr_elems == 0)
 		return NULL;
-	void* del = q->values[q->head];
+	QueueElement del = q->values[ q->head ];
+	return del;
+}
+
+/* Retrieves and removes the head of this queue,
+   or returns null if this queue is empty.
+*/
+QueueElement dequeue(Queue q) {
+	if (q->nr_elems == 0)
+		return NULL;
+	QueueElement del = q->values[ q->head ];
 	q->head++;
 	q->head %= q->size;
 	q->nr_elems--;
 	return del;
 }
 
-/* Returns the number of elements in the queue. */
+/* Returns queue current size. */
 int size(Queue q) {
 	int size = 0;
 	if (q != NULL)
 		size = q->nr_elems;
 	return size;
+}
+
+/* Returns the 1-based position where an item is on this queue.
+   Returns NOT_FOUND if the element is not on this queue.
+*/
+int search (Queue q, QueueElement value) {
+	Queue aux = q;
+    for (int i=0; i < aux->nr_elems; i++) {
+    	if ( aux->comparator( aux->values[i], value ) == 0 )
+			return i;
+    }
+    return NOT_FOUND;
+}
+
+/* Tests if this queue is empty. */
+bool empty (Queue q) {
+	return (q->nr_elems == 0);
 }
